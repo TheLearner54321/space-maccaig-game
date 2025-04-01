25c2ed36bccc4290cf699292d3cb997f8f4bcf00
TheLearner54321 <janemeighan@hotmail.com>
2025-04-01 15:49:23 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 // Phaser 3 game based on your concept
 const quotes = [
   // Assisi
@@ -265,6 +264,6 @@ function nextQuestion() {
     ans.setData('text', opt);
     ans.setData('correct', opt === currentQuote.correct);
     ans.setVelocityY(60 + (level - 1) * 20);
-    this.add.text(x - 60, 40, opt, { fontSize: '20px', fill: '#fff' });
+    this.add.text(x - 60, 120, opt, { fontSize: '20px', fill: '#fff' });
   });
 }
